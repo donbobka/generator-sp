add92c690d772e4fbc2e2e29bbb4706e9025da08
Vladimir Lyzo <vladimir.lyzo@saltpepper.ru>
2014-11-14 02:44:16 +0300

    Rewrite deploy to capistrano3 + capistrano_rsync

--- app/templates/config/deploy.rb
+++ app/templates/config/deploy.rb
@@ -1,15 +1,34 @@
-require 'capistrano/ext/multistage'
+# config valid only for Capistrano 3.2.1
+lock '3.2.1'
 
-set :stages, %w(testing production)
-set :default_stage, 'testing'
+set :application, '<%= capprojectname %>'
+set :repo_url, 'git@git.snpdev.ru:saltpepper/<%= capprojectname %>-frontend.git'
+set :scm, :rsync
 
-set :application, "<%= capprojectname %>"
-set :repository, 'dist'
+set :commit_id, ENV['CI_COMMIT_ID'] || ENV['CI_BUILD_SHA']
 
-set :deploy_to, "/var/www/#{application}/cs"
-set :use_sudo, false
-set :deploy_via, :copy
-set :scm, :none
+if fetch(:commit_id).nil?
+  ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+else
+  set :branch, fetch(:commit_id)
+end
 
-# Deploy hooks
-after 'deploy:update', 'deploy:cleanup'
+set :deploy_to, "/var/www/#{fetch(:application)}/cs"
+
+set :rsync_src_path, 'dist'
+
+set :grunt_tasks, 'build:dist'
+
+namespace :grunt do
+  task :build do
+    run_locally do
+      within fetch(:rsync_stage) do
+        execute :npm, 'install'
+        execute :bower, 'install'
+        execute :grunt, fetch(:grunt_tasks)
+      end
+    end
+  end
+end
+
+after 'rsync:stage', 'grunt:build'
